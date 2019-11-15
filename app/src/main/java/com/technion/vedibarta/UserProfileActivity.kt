fb784c7340744087f9084aa61ac3a4ed71deaf14
Sahar <saharco@campus.technion.ac.il>
2019-11-15 17:36:34 +0200

    configured app style and color. added toolbar, title picture and profile picture to gui

---
+++ app/src/main/java/com/technion/vedibarta/UserProfileActivity.kt
@@ -0,0 +1,25 @@
+package com.technion.vedibarta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_profile.*
+
+class UserProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+        initWidgets()
+    }
+
+    private fun initWidgets() {
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/technion/vedibarta/VedibartaActivity.kt
@@ -0,0 +1,12 @@
+package com.technion.vedibarta
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This is an abstract activity with no GUI
+ */
+@SuppressLint("Registered")
+class VedibartaActivity : AppCompatActivity() {
+
+}
