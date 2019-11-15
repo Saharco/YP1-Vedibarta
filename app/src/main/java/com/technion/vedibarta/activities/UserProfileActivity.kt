383d5e81e5b429c9ddf013f0fc399a2a19abd0b7
Sahar <saharco@campus.technion.ac.il>
2019-11-16 01:12:21 +0200

    added gradle dependencies for glide. fixed vector drawable support conflict. users can now upload pictures from gallery. camera upload WIP

--- app/src/main/java/com/technion/vedibarta/activities/UserProfileActivity.kt
+++ app/src/main/java/com/technion/vedibarta/activities/UserProfileActivity.kt
@@ -1,19 +1,43 @@
 package com.technion.vedibarta.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Gravity
 import kotlinx.android.synthetic.main.activity_user_profile.*
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.*
 import android.widget.TableLayout
+
+import androidx.fragment.app.DialogFragment
 import com.technion.vedibarta.R
 import com.technion.vedibarta.views.ProfilePictureUploadDialog
+import androidx.core.content.FileProvider
+import android.net.Uri
+import android.util.Log
+import java.io.File
+import android.app.Activity
+
+import android.graphics.Bitmap
+import android.os.AsyncTask
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.technion.vedibarta.utilities.RotateBitmap
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : AppCompatActivity(),
+    ProfilePictureUploadDialog.ProfilePictureUploadDialogListener {
+
+    private val REQUEST_CAMERA = 1
+    private val SELECT_IMAGE = 2
+
+    private var selectedImageFile: File? = null
+    private var selectedImage: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +53,10 @@ class UserProfileActivity : AppCompatActivity() {
         profilePicture.bringToFront()
 
         profilePicture.setOnClickListener {
-            ProfilePictureUploadDialog().show(supportFragmentManager, "UploadProfilePictureFragment")
+            ProfilePictureUploadDialog().show(
+                supportFragmentManager,
+                "UploadProfilePictureFragment"
+            )
         }
     }
 
@@ -85,5 +112,100 @@ class UserProfileActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onCameraUpload(dialog: DialogFragment) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        selectedImageFile = File(
+            externalCacheDir,
+            System.currentTimeMillis().toString() + ".jpg"
+        )
+        selectedImage = FileProvider.getUriForFile(
+            this@UserProfileActivity,
+            "$packageName.provider", selectedImageFile!!
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, selectedImage)
+        Log.d(this.localClassName, "Activating camera")
+        startActivityForResult(intent, REQUEST_CAMERA)
+    }
+
+    override fun onGalleryUpload(dialog: DialogFragment) {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(intent, SELECT_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CAMERA -> if (selectedImage != null) {
+                    uploadPhoto(selectedImage!!)
+                }
+                SELECT_IMAGE -> {
+                    selectedImage = data!!.data
+                    uploadPhoto(selectedImage!!)
+                }
+            }
+        }
+    }
+
+    private fun uploadPhoto(imagePath: Uri) {
+        val resize = ImageCompressTask()
+        resize.execute(imagePath)
+    }
+
+    /**
+     * Updates the user profile picture *IN THE DATABASE!*
+     */
+    private fun setUserProfilePic(bytes: ByteArray) {
+    }
+
+    private inner class ImageCompressTask : AsyncTask<Uri, Int, ByteArray>() {
+
+        override fun onPreExecute() {
+            profilePicturePB.visibility = View.VISIBLE
+            profilePicture.visibility = View.INVISIBLE
+        }
+
+        override fun doInBackground(vararg uris: Uri): ByteArray? {
+            try {
+                val rotateBitmap = RotateBitmap()
+                val bitmap =
+                    rotateBitmap.handleSamplingAndRotationBitmap(this@UserProfileActivity, uris[0])
+                Log.d(
+                    "UserProfileActivity",
+                    "doInBackground: MBs before compression: " + bitmap!!.byteCount.toDouble() / 1e6
+                )
+                val bytes = getBytesFromBitmap(bitmap, 80)
+                Log.d(
+                    "UserProfileActivity",
+                    "doInBackground: MBs after compression: " + bytes.size.toDouble() / 1e6
+                )
+                return bytes
+            } catch (e: IOException) {
+                Log.d("UserProfileActivity", "doInBackground: exception: $e")
+                return null
+            }
+
+        }
+
+        override fun onPostExecute(bytes: ByteArray) {
+            super.onPostExecute(bytes)
+            Glide.with(applicationContext)
+                .asBitmap()
+                .load(bytes)
+                .apply(RequestOptions.circleCropTransform())
+                .into(profilePicture)
+            profilePicturePB.visibility = View.GONE
+            profilePicture.visibility = View.VISIBLE
+            setUserProfilePic(bytes)
+        }
+
+        private fun getBytesFromBitmap(bitmap: Bitmap, quality: Int): ByteArray {
+            val stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream)
+            return stream.toByteArray()
+        }
     }
 }
--- app/src/main/java/com/technion/vedibarta/views/ProfilePictureUploadDialog.kt
+++ app/src/main/java/com/technion/vedibarta/views/ProfilePictureUploadDialog.kt
@@ -1,13 +1,24 @@
 package com.technion.vedibarta.views
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.technion.vedibarta.R
+import kotlinx.android.synthetic.main.profile_picture_dialog.*
 
 class ProfilePictureUploadDialog : DialogFragment() {
+
+    private lateinit var listener: ProfilePictureUploadDialogListener
+
+    interface ProfilePictureUploadDialogListener {
+        fun onCameraUpload(dialog: DialogFragment)
+        fun onGalleryUpload(dialog: DialogFragment)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,16 +26,35 @@ class ProfilePictureUploadDialog : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val inflatedView = inflater.inflate(R.layout.profile_picture_dialog, container, false)
-        inflatedView.setOnClickListener {
-            when (it?.id) {
-                R.id.cameraUploadButton -> TODO()
-                R.id.galleryUploadButton -> TODO()
-                R.id.dismissButton -> dismiss()
-                else -> dismiss()
-            }
-            dismiss()
-        }
         dialog?.setCanceledOnTouchOutside(true)
         return inflatedView
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = context as ProfilePictureUploadDialogListener
+        } catch (e: ClassCastException) {
+            Log.d("UploadDialog", e.toString())
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cameraUploadButton.setOnClickListener {
+            listener.onCameraUpload(this)
+            dismiss()
+        }
+
+        galleryUploadButton.setOnClickListener {
+            listener.onGalleryUpload(this)
+            dismiss()
+        }
+
+        dismissButton.setOnClickListener {
+            Toast.makeText(context, "dismiss", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
     }
 }
