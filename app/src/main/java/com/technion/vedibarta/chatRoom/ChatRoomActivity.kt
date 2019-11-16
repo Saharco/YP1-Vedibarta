898d0a2097a4cbebd63db5957443b9237fba4b96
Unknown <norgrin123@gmail.com>
2019-11-16 16:00:13 +0200

    chat room improved, has a temporary background and missing chat bubbles

--- app/src/main/java/com/technion/vedibarta/chatRoom/ChatRoomActivity.kt
+++ app/src/main/java/com/technion/vedibarta/chatRoom/ChatRoomActivity.kt
@@ -1,8 +1,11 @@
 package com.technion.vedibarta.chatRoom
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.technion.vedibarta.R
 import com.technion.vedibarta.utilities.VedibartaActivity
+import kotlinx.android.synthetic.main.activity_chat_room.*
 
 class ChatRoomActivity : VedibartaActivity() {
 
@@ -13,5 +16,21 @@ class ChatRoomActivity : VedibartaActivity() {
 
         val chatRoomListeners = ChatRoomListeners(this@ChatRoomActivity)
         chatRoomListeners.configureListeners()
+        setToolbar(chatToolbar)
+    }
+
+    private fun setToolbar(tb: Toolbar) {
+        setSupportActionBar(tb)
+        supportActionBar?.setDisplayShowTitleEnabled(false) // if you want to to write your own title programmatically
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+            else -> TODO()
+        }
+        return true
     }
 }
