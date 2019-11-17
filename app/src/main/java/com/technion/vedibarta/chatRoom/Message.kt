08cba1a85f539eda7af2d9d5bb6bc206d22adbaa
Unknown <norgrin123@gmail.com>
2019-11-17 12:04:24 +0200

    fixed timeStamp

--- app/src/main/java/com/technion/vedibarta/chatRoom/Message.kt
+++ app/src/main/java/com/technion/vedibarta/chatRoom/Message.kt
@@ -1,12 +1,14 @@
 package com.technion.vedibarta.chatRoom
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 class Message
 {
     var userId: Int = 0
     var text: String? = null
-    var timestamp = Date(System.currentTimeMillis()).toString()
+    private val formatter = SimpleDateFormat("HH:mma")
+    var timestamp = formatter.format(Date(System.currentTimeMillis()))
 
     constructor() //empty for firebase
 
