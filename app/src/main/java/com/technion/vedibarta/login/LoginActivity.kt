7eb70364a8051147e39e79d7b9d3dcd3259c8303
Or Borenstein <borenstein@campus.technion.ac.il>
2019-11-21 19:19:20 +0200

    created initial version of sign-in options screen layout

--- app/src/main/java/com/technion/vedibarta/login/LoginActivity.kt
+++ app/src/main/java/com/technion/vedibarta/login/LoginActivity.kt
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.technion.vedibarta.R
 
+
+
+
+
+
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val fm = supportFragmentManager
+        fm.beginTransaction().apply {
+            replace(R.id.login_screen_fragment, LoginOptionsFragment())
+        }.commit()
     }
 }
