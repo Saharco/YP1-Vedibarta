1d435c3f46a3a39af7cc34f5cb8b21e6ccce23c5
Or Borenstein <borenstein@campus.technion.ac.il>
2019-11-22 20:47:38 +0200

    created initial version of login screen layout

--- app/src/main/java/com/technion/vedibarta/login/LoginOptionsFragment.kt
+++ app/src/main/java/com/technion/vedibarta/login/LoginOptionsFragment.kt
@@ -1,21 +1,44 @@
 package com.technion.vedibarta.login
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.technion.vedibarta.R
+import java.lang.ClassCastException
 
 
 class LoginOptionsFragment : Fragment() {
+    lateinit var signInListener : OnSignInButtonClickListener
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_options, container, false)
+        val view = inflater.inflate(R.layout.fragment_login_options, container, false)
+
+        val loginButton = view.findViewById<Button>(R.id.sign_in_link)
+        loginButton.setOnClickListener { signIn() }
+
+        return view
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        signInListener = context as? OnSignInButtonClickListener ?:
+                throw ClassCastException("$context must implement OnSignInButtonClickListener")
+    }
+
+    private fun signIn(){
+        signInListener.onSignInButtonClick()
+    }
+
+    interface OnSignInButtonClickListener {
+        fun onSignInButtonClick()
+    }
 }
