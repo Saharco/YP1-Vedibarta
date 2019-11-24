eff26f9ceb8c3b213966b04a6552efaff4b5a95b
Sahar <saharco@campus.technion.ac.il>
2019-11-24 18:38:58 +0200

    finished demo carousel with placeholder views

---
+++ app/src/main/java/com/technion/vedibarta/adapters/CarouselAdapter.kt
@@ -0,0 +1,43 @@
+package com.technion.vedibarta.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.recyclerview.widget.RecyclerView
+import com.technion.vedibarta.R
+import kotlinx.android.synthetic.main.carousel_item.view.*
+
+class CarouselAdapter(val itemClick: (position:Int, item: Item) -> Unit) : RecyclerView.Adapter<ItemViewHolder>() {
+
+    private var items: List<Item> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
+        ItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.carousel_item, parent, false))
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(items[position])
+        holder.itemView.setOnClickListener {
+            itemClick(position,items[position])
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    fun setItems(newItems: List<Item>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+}
+
+class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(item: Item) {
+        view.listItemText.text = item.title
+        view.listItemIcon.setImageResource(item.icon)
+    }
+}
+
+data class Item(
+    val title: String,
+    @DrawableRes val icon: Int
+)
