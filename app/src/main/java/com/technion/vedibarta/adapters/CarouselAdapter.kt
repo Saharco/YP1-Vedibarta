2d3d79d803c2ce01b3bf3f392dc899333c2b20a6
Sahar <saharco@campus.technion.ac.il>
2019-11-25 22:59:45 +0200

    changed carousel items layout to better fit different types of screen. added landscape layouts for the items

--- app/src/main/java/com/technion/vedibarta/adapters/CarouselAdapter.kt
+++ app/src/main/java/com/technion/vedibarta/adapters/CarouselAdapter.kt
@@ -1,5 +1,7 @@
 package com.technion.vedibarta.adapters
 
+import android.content.Context
+import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,18 +9,35 @@ import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.RecyclerView
 import com.technion.vedibarta.R
 
-class CarouselAdapter(val itemClick: (position:Int, carouselAdapterItem: CarouselAdapterItem) -> Unit) : RecyclerView.Adapter<ItemViewHolder>() {
+class CarouselAdapter(val context: Context, val itemClick: (position: Int, carouselAdapterItem: CarouselAdapterItem) -> Unit) :
+    RecyclerView.Adapter<ItemViewHolder>() {
 
     private var carouselAdapterItems: List<CarouselAdapterItem> = listOf()
     private val selectedPos = RecyclerView.NO_POSITION
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
-        ItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.carousel_item, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val orientation = context.resources.configuration.orientation
+        return if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.carousel_item_horizontal,
+                    parent,
+                    false
+                ))
+        } else {
+            ItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.carousel_item,
+                    parent,
+                    false
+                ))
+        }
+    }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.bind(carouselAdapterItems[position])
         holder.itemView.setOnClickListener {
-            itemClick(position,carouselAdapterItems[position])
+            itemClick(position, carouselAdapterItems[position])
         }
         holder.itemView.isSelected = position == selectedPos
     }
