8bce7b679eec69e1fcd4ed546e057778acfb2587
Or Borenstein <borenstein@campus.technion.ac.il>
2019-11-25 16:30:41 +0200

    Refactoring: moved google authentication logic from LoginOptionsFragment to LoginActivity

--- app/src/main/java/com/technion/vedibarta/login/LoginActivity.kt
+++ app/src/main/java/com/technion/vedibarta/login/LoginActivity.kt
@@ -1,17 +1,45 @@
 package com.technion.vedibarta.login
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.technion.vedibarta.R
 
 
+private const val REQ_GOOGLE_SIGN_IN = 1
+private const val TAG = "LoginActivity"
+
 class LoginActivity : AppCompatActivity(), LoginOptionsFragment.OnSignInButtonClickListener,
     LoginOptionsFragment.OnSignUpWithEmailButtonClickListener,
+    LoginOptionsFragment.OnContinueWithGoogleButtonClickListener,
     LoginFragment.OnBackButtonClickListener, SignUpWithEmailFragment.OnBackButtonClickListener {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        // Set up google sign-in client
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         // If activity is being restored from a previous state, then do nothing.
         if(savedInstanceState != null) {
@@ -24,11 +52,23 @@ class LoginActivity : AppCompatActivity(), LoginOptionsFragment.OnSignInButtonCl
         }.commit()
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        updateUIForCurrentUser(currentUser)
+    }
+
     override fun onSignInButtonClick() {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.login_screen_fragment, LoginFragment())
             addToBackStack(null)
         }.commit()
+    }
+
+    override fun onContinueWithGoogleButtonClick() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, REQ_GOOGLE_SIGN_IN)
     }
 
     override fun onSignUpWithEmailButtonClick() {
@@ -40,5 +80,65 @@ class LoginActivity : AppCompatActivity(), LoginOptionsFragment.OnSignInButtonCl
 
     override fun onBackButtonClick() {
         supportFragmentManager.popBackStack()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data == null) {
+            Log.w(TAG, "Intent is null")
+            return
+        }
+
+        if (requestCode == REQ_GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Failed to get account from intent")
+            }
+
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+            }
+
+            return
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle: ${account.id!!}")
+
+        val dialog = ProgressDialog(this).apply {
+            setMessage("Loading data...")
+            setCancelable(false)
+            setIndeterminate(true)
+            show()
+        }
+
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.w(TAG, "signInWithCredential: success")
+                    val user = auth.currentUser
+                    updateUIForCurrentUser(user)
+                } else {
+                    // Sign in failed
+                    Log.w(TAG, "signInWithCredential: failure", task.exception)
+                    Toast.makeText(this, "Auth Failed", Toast.LENGTH_LONG).show()
+                }
+
+                dialog.dismiss()
+            }
+    }
+
+    private fun updateUIForCurrentUser(user: FirebaseUser?) {
+        if (user != null) {
+            Toast.makeText(this, "Hello, ${user.displayName}!", Toast.LENGTH_LONG).show()
+            // TODO: check if the user's document exists.
+            //  If so, direct to main screen. Otherwise, direct to profile creation screen.
+        }
     }
 }
