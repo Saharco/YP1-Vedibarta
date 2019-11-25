f4a0eac9d272ccbcdc469fb26fc1370348d9bcd0
Sahar <saharco@campus.technion.ac.il>
2019-11-26 01:56:40 +0200

    binding views into carousel wip

--- app/src/main/java/com/technion/vedibarta/adapters/CarouselAdapter.kt
+++ app/src/main/java/com/technion/vedibarta/adapters/CarouselAdapter.kt
@@ -2,17 +2,32 @@ package com.technion.vedibarta.adapters
 
 import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.technion.vedibarta.POJOs.Student
 import com.technion.vedibarta.R
+import com.technion.vedibarta.utilities.Gender
+import com.technion.vedibarta.utilities.VedibartaActivity
+import kotlinx.android.synthetic.main.activity_user_profile.*
 
-class CarouselAdapter(val context: Context, val itemClick: (position: Int, carouselAdapterItem: CarouselAdapterItem) -> Unit) :
+class CarouselAdapter(val context: Context, val itemClick: (position: Int, carouselAdapterItem: Student) -> Unit) :
     RecyclerView.Adapter<ItemViewHolder>() {
 
-    private var carouselAdapterItems: List<CarouselAdapterItem> = listOf()
+    private var carouselAdapterItems: List<Student> = listOf()
     private val selectedPos = RecyclerView.NO_POSITION
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -40,23 +55,70 @@ class CarouselAdapter(val context: Context, val itemClick: (position: Int, carou
             itemClick(position, carouselAdapterItems[position])
         }
         holder.itemView.isSelected = position == selectedPos
+
+        val student = carouselAdapterItems[position]
+        loadProfilePicture(holder.profilePicture, student)
+        holder.name.text = student.name
+        holder.description.text = "${student.school}, ${student.region}"
     }
+
+    private fun loadProfilePicture(profilePicture: ImageView, student: Student) {
+        Glide.with(context)
+            .asBitmap()
+            .load(student.photo)
+            .apply(RequestOptions.circleCropTransform())
+            .listener(object : RequestListener<Bitmap> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    loadDefaultUserProfilePicture(profilePicture, student)
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+//                    displayUserProfilePicture(profilePicture)
+                    return false
+                }
+
+            })
+            .into(profilePicture)
+    }
+
+    private fun loadDefaultUserProfilePicture(profilePicture: ImageView, student: Student) {
+        if (student.gender == Gender.MALE)
+            profilePicture.setImageResource(R.drawable.ic_photo_default_profile_man)
+        else
+            profilePicture.setImageResource(R.drawable.ic_photo_default_profile_girl)
+//        displayUserProfilePicture(profilePicture)
+    }
+
 
     override fun getItemCount() = carouselAdapterItems.size
 
-    fun setItems(newCarouselAdapterItems: List<CarouselAdapterItem>) {
+    fun  setItems(newCarouselAdapterItems: List<Student>) {
         carouselAdapterItems = newCarouselAdapterItems
         notifyDataSetChanged()
     }
 }
 
 class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-    fun bind(carouselAdapterItem: CarouselAdapterItem) {
-        //TODO: bind the views here
+
+    val profilePicture: ImageView = view.findViewById(R.id.candidateProfilePicture)
+    val name : TextView = view.findViewById(R.id.candidateName)
+    val description : TextView = view.findViewById(R.id.candidateDescription)
+    val table : TableLayout = view.findViewById(R.id.candidateTable)
+    val button : Button = view.findViewById(R.id.confirmCandidateButton)
+
+    fun bind(carouselAdapterItem: Student) {
+        //TODO: add listeners, tags, etc here
     }
 }
-
-data class CarouselAdapterItem(
-    val title: String,
-    @DrawableRes val icon: Int
-)
--- app/src/main/java/com/technion/vedibarta/chatCandidates/ChatCandidatesActivity.kt
+++ app/src/main/java/com/technion/vedibarta/chatCandidates/ChatCandidatesActivity.kt
@@ -4,22 +4,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.technion.vedibarta.R
 import com.technion.vedibarta.adapters.CarouselAdapter
-import com.technion.vedibarta.adapters.CarouselAdapterItem
+import com.technion.vedibarta.POJOs.Student
+import com.technion.vedibarta.utilities.Gender
+import com.technion.vedibarta.utilities.VedibartaActivity
 import kotlinx.android.synthetic.main.activity_chat_candidates.*
+import java.sql.Timestamp
 
-class ChatCandidatesActivity : AppCompatActivity() {
+class ChatCandidatesActivity : VedibartaActivity() {
 
     private val TAG = "ChatCandidates"
 
-    private val carouselAdapter = CarouselAdapter(this) { position: Int, _: CarouselAdapterItem ->
-        carousel.smoothScrollToPosition(position)}
+    private val carouselAdapter = CarouselAdapter(this) { position: Int, _: Student ->
+        carousel.smoothScrollToPosition(position)
+    }
 
     private val possibleItems = listOf(
-        CarouselAdapterItem("Camera", R.drawable.ic_camera_blue),
-        CarouselAdapterItem("Upload", R.drawable.ic_upload_blue),
-        CarouselAdapterItem("Gallery", R.drawable.ic_gallery_blue),
-        CarouselAdapterItem("Check", R.drawable.ic_check_white),
-        CarouselAdapterItem("Edit", R.drawable.ic_edit_white)
+        student!!,
+        Student(
+            "יובל נהון",
+            "https://firebasestorage.googleapis.com/v0/b/takecare-81dab.appspot.com/o/itemPictures%2FuserUploads%2F1hrI3pYVvrVbtnxzrRh8nH3A1Y92%2F1aa42704-181f-4698-91ec-166d093902ae?alt=media&token=0fef3b51-16f2-4918-b8ae-b5ff1dacf5eb",
+            "נתניה",
+            "אורט יד ליבוביץ'",
+            Gender.MALE,
+            Timestamp(System.currentTimeMillis()),
+            arrayOf("מסורתי", "מזרחי", "צבר"),
+            arrayOf("ספורט", "לגלוש", "רובוטיקה")
+        ),
+        Student(
+            "ויקטור בניאס",
+            "https://firebasestorage.googleapis.com/v0/b/takecare-81dab.appspot.com/o/itemPictures%2FuserUploads%2FjTj93R9w05WEVQoqLPLfU4BSjDl1%2Fc0f3aac9-08e1-437a-b9c3-4dd8217f0f16?alt=media&token=715bdec5-8a32-4b82-8f75-aa25bc179f4b",
+            "טבריה",
+            "עמל נופרים בגליל",
+            Gender.MALE,
+            Timestamp(System.currentTimeMillis()),
+            arrayOf("צבר", "רוסי", "חילוני", "עולה חדש"),
+            arrayOf("ספורט", "לבשל", "בולונז")
+        ),
+        Student(
+            "גיל צימרמן",
+            null,
+            "יקנעם",
+            "טכניון",
+            Gender.MALE,
+            Timestamp(System.currentTimeMillis()),
+            arrayOf("יהודי"),
+            arrayOf()
+        ),
+        Student(
+            "שרה כהן",
+            null,
+            "עמק חפר",
+            "רמות ים",
+            Gender.FEMALE,
+            Timestamp(System.currentTimeMillis()),
+            arrayOf(),
+            arrayOf()
+        )
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +70,8 @@ class ChatCandidatesActivity : AppCompatActivity() {
         carouselAdapter.setItems(getLargeListOfItems())
     }
 
-    private fun getLargeListOfItems(): List<CarouselAdapterItem> {
-        val items = mutableListOf<CarouselAdapterItem>()
+    private fun getLargeListOfItems(): List<Student> {
+        val items = mutableListOf<Student>()
         (0..40).map { items.add(possibleItems.random()) }
         return items
     }
