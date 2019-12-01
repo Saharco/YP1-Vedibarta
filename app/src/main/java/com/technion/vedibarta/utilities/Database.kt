f00768a79f45405ffb3f276075ca9a952bb5c8ce
Unknown <norgrin123@gmail.com>
2019-12-01 13:03:37 +0200

    added upload file method, yet to be tested

--- app/src/main/java/com/technion/vedibarta/utilities/Database.kt
+++ app/src/main/java/com/technion/vedibarta/utilities/Database.kt
@@ -1,7 +1,7 @@
 package com.technion.vedibarta.utilities
 
+import android.net.Uri
 import android.util.Log
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -14,7 +14,9 @@ class Database
     private val database = FirebaseFirestore.getInstance()
     private val storage = FirebaseStorage.getInstance().reference
     private val user = FirebaseAuth.getInstance().currentUser
-    private val studentsCollection = "students"
+    private val studentsDocumentsCollection = "students"
+    private val studentsProfileImagesStorage = "students/profile_pictures"
+
 
     fun saveStudentProfile(name: String, photo: String?, region: String, shcool: String, gender: Gender,
                             lastTimeActive: Timestamp, characteristics: List<String>, hobbies: List<String>)
@@ -23,13 +25,24 @@ class Database
 
         try {
             if (user != null)
-                database.collection(studentsCollection).document(user.uid).set(s)
+                database.collection(studentsDocumentsCollection).document(user.uid).set(s)
                     .addOnSuccessListener { Log.d("Database", "saved profile of ${user.uid}")}
                     .addOnFailureListener { e: Exception -> Log.d("Database", "${user.uid} got error: ${e.message}") }
         }
         catch (e: Exception)
         {
             Log.d("Database", "${user?.uid} got error: ${e.message}")
+        }
+    }
+
+    fun uploadFile(file: Uri, path: String)
+    {
+        if (user != null)
+        {
+            val photoPathRef = storage.child(path)
+            val uploadTask = photoPathRef.putFile(file)
+            uploadTask.addOnSuccessListener { Log.d("Database", "${user.uid} uploaded a file") }
+                .addOnFailureListener{ e: Exception -> Log.d("Database", "${user.uid} got error: ${e.message}") }
         }
     }
 }
