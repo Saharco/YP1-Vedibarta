8c975e81d526008e8af2350345e3d58cea43581d
Unknown <norgrin123@gmail.com>
2019-12-08 11:19:48 +0200

    chatAdapter works

---
+++ app/src/main/java/com/technion/vedibarta/chatRoom/ChatRoomListeners.kt
@@ -0,0 +1,66 @@
+package com.technion.vedibarta.chatRoom
+
+import android.app.Activity
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
+import androidx.fragment.app.FragmentManager
+import com.technion.vedibarta.POJOs.Message
+import com.technion.vedibarta.POJOs.MessageType
+import com.technion.vedibarta.R
+import com.technion.vedibarta.utilities.ListenersSetter
+import com.technion.vedibarta.utilities.VedibartaActivity
+
+class ChatRoomListeners(private val chatRoom: ChatRoomActivity, private val supportFragmentManager: FragmentManager) {
+
+    fun configureListeners() {
+        val popupMenu = chatRoom.findViewById<View>(R.id.popupMenu)
+        val sendButton = chatRoom.findViewById<View>(R.id.buttonChatBoxSend)
+        val setter = ListenersSetter()
+        setter.setListener(popupMenu, this::showPopup)
+        setter.setListener(sendButton,this::sendMessage)
+    }
+
+    private fun showPopup(view: View) {
+        val popup = PopupMenu(chatRoom, view)
+        popup.inflate(R.menu.chat_room_popup_menu)
+
+        popup.setOnMenuItemClickListener { item: MenuItem? ->
+
+            when (item!!.itemId) {
+                R.id.generateQuestion -> {
+                        ChatRoomQuestionGeneratorDialog().show(
+                            supportFragmentManager,
+                            "QuestionGeneratorFragment"
+                        )
+                }
+                R.id.reportAbuse -> {
+                    ChatRoomAbuseReportDialog().show(
+                        supportFragmentManager,
+                        "ReportAbuseDialog"
+                    )
+                }
+            }
+
+            true
+        }
+        popup.show()
+    }
+
+    private fun sendMessage(v: View)
+    {
+        val chatBox: EditText = chatRoom.findViewById<View>(R.id.chatBox) as EditText
+
+        chatRoom.database
+            .students()
+            .userId()
+            .chats()
+            .build().add(Message(text = chatBox.text.toString()))
+            .addOnSuccessListener { chatBox.setText("")}
+            .addOnFailureListener {
+                Toast.makeText(chatRoom, R.string.something_went_wrong, Toast.LENGTH_LONG).show() }
+    }
+
+}
