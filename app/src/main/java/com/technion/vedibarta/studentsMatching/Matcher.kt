fd5e7da655ea33fe354847621e2ea904c746a980
Or Borenstein <borenstein@campus.technion.ac.il>
2019-12-08 22:33:47 +0200

    added external documentation

--- app/src/main/java/com/technion/vedibarta/studentsMatching/Matcher.kt
+++ app/src/main/java/com/technion/vedibarta/studentsMatching/Matcher.kt
@@ -3,8 +3,19 @@ package com.technion.vedibarta.studentsMatching
 import com.google.firebase.firestore.DocumentSnapshot
 import com.technion.vedibarta.POJOs.Student
 
-
+/**
+ * An interface for a matching algorithm between students.
+ */
 interface Matcher {
+    /**
+     * Run the matching algorithm and return the matched students.
+     * @return a [List] of [Student] objects, the matched students.
+     */
     fun match(): List<Student>
+
+    /**
+     * Run the matching algorithm and return the documents of the matched students.
+     * @return a [List] of [DocumentSnapshot], the matched students' documents at time of running.
+     */
     fun getDocs(): List<DocumentSnapshot>
 }
