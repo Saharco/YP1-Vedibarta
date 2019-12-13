4155a6bff834291f0d332e3eb3e32a27b7a51cf7
Sahar <saharco@campus.technion.ac.il>
2019-12-14 01:39:18 +0200

    initialized cloud functions. added onMessageSent function: tracking counter and last message timestamp in a chat room. added onMessageSentNotify: sends a notification payload to a user. untested! need to add messaging service and tokens

---
+++ app/src/main/java/com/technion/vedibarta/utilities/services/VedibartaMessagingService.kt
@@ -0,0 +1,8 @@
+package com.technion.vedibarta.utilities.services
+
+import android.R
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class VedibartaMessagingService : FirebaseMessagingService() {
+    //TODO: need to implement the service logic
+}
