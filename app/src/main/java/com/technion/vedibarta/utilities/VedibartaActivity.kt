92d6fcaf4baddcd5bc65c5a48edfacf1d518b429
Sahar <saharco@campus.technion.ac.il>
2019-12-14 15:55:01 +0200

    working on messaging service - implementing the sendChatNotification method. added utility methods to VedibartaActivity

--- app/src/main/java/com/technion/vedibarta/utilities/VedibartaActivity.kt
+++ app/src/main/java/com/technion/vedibarta/utilities/VedibartaActivity.kt
@@ -2,31 +2,79 @@ package com.technion.vedibarta.utilities
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
 import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Build
-import android.os.Bundle
+import android.os.Handler
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.technion.vedibarta.POJOs.Student
-import java.sql.Timestamp
-import java.util.*
+import com.technion.vedibarta.R
+import com.technion.vedibarta.login.LoginActivity
 
 /**
  * This is a utility activity with no GUI
  */
 @SuppressLint("Registered")
-open class VedibartaActivity : AppCompatActivity()
-{
-    val userId = FirebaseAuth.getInstance().currentUser?.uid
-    val storage = Storage(userId)
-    val database = DocumentsCollections(userId)
-    //TODO: for now this will configure a default student. change this to null later
+open class VedibartaActivity : AppCompatActivity() {
+    protected val userId = FirebaseAuth.getInstance().currentUser?.uid
+    protected val storage = Storage(userId)
+    protected val database = DocumentsCollections(userId)
+
+    private var progress: ProgressDialog? = null
+    private var progressHandler: Handler? = null
+    protected var loadTimeout: Int = 5000
+
+    // A task that's executed when a time-out occurs with a loading process
+    private val loadTimeoutTask = Runnable {
+
+        if (progress != null) {
+            progress!!.dismiss()
+        }
+
+        val title = TextView(this)
+        title.setText(R.string.dialog_timeout_title)
+        title.textSize = 20f
+        title.setTypeface(null, Typeface.BOLD)
+        title.setTextColor(resources.getColor(R.color.textPrimary))
+        title.gravity = Gravity.CENTER
+        title.setPadding(10, 40, 10, 24)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setCustomTitle(title)
+            .setIcon(R.drawable.ic_warning_yellow)
+            .setMessage(R.string.dialog_timeout_body)
+            .setNeutralButton(android.R.string.ok) { _, _ -> onBackPressed() }
+        val dialog = builder.create()
+        dialog.show()
+
+        // Display "OK" button in the middle
+        val neutralButton = dialog.getButton(AlertDialog.BUTTON_NEUTRAL)
+        val buttonLayoutParams =
+            neutralButton.layoutParams as LinearLayout.LayoutParams
+        buttonLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+        neutralButton.layoutParams = buttonLayoutParams
+    }
+
     companion object {
         var student: Student? = null
+        var chatPartnerId: String? = null
+        var isActivityRunning = false
+
+        const val IMAGE_COMPRESSION_QUALITY_IN_PERCENTS = 90
 
         /**
          * @param resources: resources object of the current context
@@ -54,12 +102,30 @@ open class VedibartaActivity : AppCompatActivity()
 
     }
 
-    protected val IMAGE_COMPRESSION_QUALITY_IN_PERCENTS: Int = 90
-
     override fun onStart() {
         super.onStart()
+        tryRedirectToLogin()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tryRedirectToLogin()
+        isActivityRunning = true
+        setVisible(true)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isActivityRunning = false
+        setVisible(false)
+    }
+
+    private fun tryRedirectToLogin() {
         if (student == null) {
-            //TODO: redirect to LoginActivity here!!!
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
         }
     }
 
@@ -82,6 +148,51 @@ open class VedibartaActivity : AppCompatActivity()
         }
     }
 
+    /**
+     * Shows a highlighted text (colored in yellow) in a snackbar.
+     * Used mainly to notify the user about errors
+     *
+     * @param root: the context's root element (activity's root layout)
+     * @param msg:  the message displayed in the snackbar
+     */
+    protected open fun makeHighlightedSnackbar(root: View, msg: String) {
+        val snackbar = Snackbar.make(root, msg, Snackbar.LENGTH_SHORT)
+        val textView =
+            snackbar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+        textView.setTextColor(Color.YELLOW)
+        snackbar.show()
+    }
 
+    /**
+     * Blocks user interaction and displays a progress dialog.
+     * After a timeout, progress dialog is dismissed and an alert dialog pops up to inform the user
+     * about an error.
+     * This method should be invoked when some asynchronous task is running in the background
+     *
+     * @param msg:     message to display to the user while loading
+     * @param timeout: amount of time (in milliseconds) before the progress dialog is dismissed
+     */
+    protected open fun startLoading(msg: String?, timeout: Int?) {
+        progress = ProgressDialog(this)
+        progress!!.setCancelable(false)
+        progress!!.isIndeterminate = true
+        progress!!.setMessage(msg)
+        progress!!.show()
+        if (progressHandler == null) {
+            progressHandler = Handler()
+        }
+        progressHandler!!.removeCallbacks(loadTimeoutTask)
+        val loadTime =
+            timeout ?: loadTimeout
+        progressHandler!!.postDelayed(loadTimeoutTask, loadTime.toLong())
+    }
 
+    /**
+     * Dismisses a loading progress dialog. Should be called after [startLoading]
+     */
+    protected open fun stopLoading() {
+        if (progress == null || !progress!!.isShowing) return
+        progress!!.dismiss()
+        progressHandler?.removeCallbacks(loadTimeoutTask)
+    }
 }
--- app/src/main/java/com/technion/vedibarta/utilities/services/VedibartaMessagingService.kt
+++ app/src/main/java/com/technion/vedibarta/utilities/services/VedibartaMessagingService.kt
@@ -1,24 +1,60 @@
 package com.technion.vedibarta.utilities.services
 
-import android.R
+import android.util.Log
+import com.technion.vedibarta.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+/**
+ * This background service is constantly running on the user's device and listens to cloud functions' payloads
+ * that are sent to a user's FCM token.
+ * The service also handles new new tokens.
+ *
+ * //FIXME: this class is not yet complete
+ */
 class VedibartaMessagingService : FirebaseMessagingService() {
 
     companion object {
         private val TAG = "vedibarta/messaging"
     }
 
-    //TODO: need to implement the service logic
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        TODO()
+        Log.d(TAG, "#onMessageReceived: started with message: ${remoteMessage.data}")
+        val payloadDisplayStatus =
+            remoteMessage.data[getString(R.string.payload_display_status)] ?: return
+
+        if (payloadDisplayStatus == getString(R.string.payload_admin_broadcast)) {
+            Log.d(TAG, "#onMessageReceived: notification is an admin notification")
+            buildAdminBroadcastNotification(remoteMessage.data)
+        }
     }
 
-    private fun sendChatNotification(title:String, message:String, senderId:String,
-                                     senderPhotoURL:String?, chatId:String) {
-        TODO()
+    private fun buildAdminBroadcastNotification(messageData: Map<String, String>) {
+        val notificationType =
+            messageData[getString(R.string.payload_data_notification_type)] ?: return
+        val title = messageData.getValue(getString(R.string.payload_data_message_title))
+        val messageBody = messageData.getValue(getString(R.string.payload_data_message_body))
+
+        when (notificationType) {
+            // possibly add more notification types in the future. for now, there's only chat
+            "CHAT" -> {
+                val senderId = messageData.getValue(getString(R.string.payload_data_sender_id))
+                val chatId = messageData.getValue(getString(R.string.payload_data_chat_id))
+                val senderPhotoURL = messageData.getValue(getString(R.string.payload_data_sender_photo))
+                sendChatNotification(title, messageBody, senderId, senderPhotoURL, chatId)
+            }
+            else -> return
+        }
+    }
+
+    private fun sendChatNotification(
+        title: String, message: String, senderId: String,
+        senderPhotoURL: String?, chatId: String
+    ) {
+        Log.d(TAG, "#sendChatNotification: starting...")
+        Log.d(TAG, "#sendChatNotification: checking if the user is currently in an active chat with the sender")
+
+
     }
 
     override fun onNewToken(p0: String) {
