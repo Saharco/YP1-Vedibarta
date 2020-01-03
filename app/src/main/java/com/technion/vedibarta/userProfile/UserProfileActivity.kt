4897b802f5c0e8ecfefe8b481a1d548c1e6c0c06
Unknown <norgrin123@gmail.com>
2020-01-03 15:32:12 +0200

    validateImage returns false if labeled as Flesh or Skin or Swimwear

--- app/src/main/java/com/technion/vedibarta/userProfile/UserProfileActivity.kt
+++ app/src/main/java/com/technion/vedibarta/userProfile/UserProfileActivity.kt
@@ -726,6 +726,10 @@ class UserProfileActivity : VedibartaActivity(),
                 {
                     Log.d("wtf", "Label: ${label.text} confidence: ${label.confidence}")
                 }
+                val labels = labeler.result!!.map { it.text }
+                val res = labels.intersect(listOf("Flesh", "Skin", "Swimwear")).isEmpty()
+                Log.d("wtf", "res = $res")
+                return res
             }
         }
         catch (e: Exception)
@@ -733,7 +737,7 @@ class UserProfileActivity : VedibartaActivity(),
             Log.d(TAG, "validateImage ${e.message}, cause: ${e.cause?.message}")
         }
 
-        return true
+        return false
     }
 
     private fun uploadPhoto(imagePath: Uri) {
