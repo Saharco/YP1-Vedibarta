985b9dbc984b971c5cd09c8c6bce95a22e2368a1
Or Borenstein <borenstein@campus.technion.ac.il>
2020-01-20 16:37:19 +0200

    moved to production database version

--- app/src/main/java/com/technion/vedibarta/database/DatabaseVersion.kt
+++ app/src/main/java/com/technion/vedibarta/database/DatabaseVersion.kt
@@ -4,8 +4,8 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 object DatabaseVersioning {
-    private val VERSION_TYPE = VersionType.development
-    private const val VERSION_NUMBER = "1.0.1"
+    private val VERSION_TYPE = VersionType.production
+    private const val VERSION_NUMBER = "1.0.0"
 
     val currentVersion = RegularVersion(VERSION_TYPE, VERSION_NUMBER)
     fun getTestVersion(name: String) = TestVersion(name)
