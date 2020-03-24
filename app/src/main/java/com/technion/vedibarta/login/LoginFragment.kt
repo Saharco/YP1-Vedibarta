3a13e102c351f06584e88b7ab8498b2f558374d9
Or Borenstein <borenstein@campus.technion.ac.il>
2020-03-24 18:06:01 +0200

    Added comments to LoginFragment

--- app/src/main/java/com/technion/vedibarta/login/LoginFragment.kt
+++ app/src/main/java/com/technion/vedibarta/login/LoginFragment.kt
@@ -18,7 +18,11 @@ import kotlinx.android.synthetic.main.fragment_sign_in_with_email.*
 import java.lang.ClassCastException
 
 /**
- * A simple [Fragment] subclass.
+ * The sign-in with email screen.
+ *
+ * This class handles the actions taken when any button is pressed on the sign-in with email screen.
+ *
+ * @see R.layout.fragment_sign_in_with_email
  */
 class LoginFragment : Fragment() {
     // Keys to be used when saving and restoring fragment states.
@@ -28,6 +32,7 @@ class LoginFragment : Fragment() {
     }
 
     private lateinit var backListener: OnBackButtonClickListener
+    // Notice: the listener will be called only after checking for valid inputs.
     private lateinit var loginListener: OnLoginButtonClickListener
 
     override fun onAttach(context: Context) {
@@ -45,18 +50,22 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_sign_in_with_email, container, false)
-
+        // Set up back button listener.
         val backButton = view.findViewById<Button>(R.id.back_button)
         backButton.setOnClickListener { back() }
-
+        // Set up login button listener.
         val loginButton = view.findViewById<Button>(R.id.login_button)
-        loginButton.setOnClickListener { login() }
+        loginButton.setOnClickListener {
+            // Checking input before calling loginListener.
+            login()
+        }
 
         val email = view.findViewById<TextInputEditText>(R.id.email_input_edit_text)
         val emailLayout = view.findViewById<TextInputLayout>(R.id.email_input_layout)
         val password = view.findViewById<TextInputEditText>(R.id.password_input_edit_text)
         val passwordLayout = view.findViewById<TextInputLayout>(R.id.password_input_layout)
 
+        // Removing error messages after editing texts.
         email.addTextChangedListener { emailLayout.error = null }
         password.addTextChangedListener { passwordLayout.error = null }
 
@@ -66,6 +75,7 @@ class LoginFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        // Refilling edit-texts when restarting the activity.
         if (savedInstanceState != null) {
             email_input_edit_text.setText(savedInstanceState.getString(EMAIL_KEY))
             password_input_edit_text.setText(savedInstanceState.getString(PASSWORD_KEY))
@@ -75,6 +85,7 @@ class LoginFragment : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
+        // Saving filled texts.
         outState.putString(email_input_edit_text.text.toString(), EMAIL_KEY)
         outState.putString(password_input_edit_text.text.toString(), PASSWORD_KEY)
     }
@@ -101,6 +112,7 @@ class LoginFragment : Fragment() {
         email_input_layout.error = emailError
         password_input_layout.error = passwordError
 
+        // Calling loginListener only if there were no errors.
         if ((emailError == null) and (passwordError == null)){
             loginListener.onLoginButtonClick(email, password)
         }
