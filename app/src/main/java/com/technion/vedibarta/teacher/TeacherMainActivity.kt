fdc68a24400158558012e3cae5ae065b68bb3be6
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2020-03-31 20:02:22 +0300

    TeacherHomeFragment was added. dummy TeacherProfileFragment was added. TeacherMainActivity can swap fragments.

--- app/src/main/java/com/technion/vedibarta/teacher/TeacherMainActivity.kt
+++ app/src/main/java/com/technion/vedibarta/teacher/TeacherMainActivity.kt
@@ -1,13 +1,36 @@
 package com.technion.vedibarta.teacher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.technion.vedibarta.R
 
-class TeacherMainActivity : AppCompatActivity() {
-
+class TeacherMainActivity : FragmentActivity() {
+    lateinit var bottomNavigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher_main)
+        openFragment(TeacherHomeFragment.newInstance("", ""))
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> openFragment(TeacherHomeFragment.newInstance("", ""))
+                R.id.profile -> openFragment(TeacherProfileFragment.newInstance("",""))
+            }
+            true
+        }
+    }
+
+    fun openFragment(fragment: Fragment?) {
+        if (fragment != null) {
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.container, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 }
+
