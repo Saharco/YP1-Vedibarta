c2c7adf37eb2aefb6c7f4f0ed4e3fd9b3eb7ed3b
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2020-03-31 18:40:31 +0300

    minor fixes to the manifest after the merge

---
+++ app/src/main/java/com/technion/vedibarta/teacher/TeacherMainActivity.kt
@@ -0,0 +1,13 @@
+package com.technion.vedibarta.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.technion.vedibarta.R
+
+class TeacherMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_main)
+    }
+}
