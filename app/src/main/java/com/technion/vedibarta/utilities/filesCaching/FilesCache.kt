30973ac6718da03cdc693992285b169e5c3f80e4
Or Borenstein <borenstein@campus.technion.ac.il>
2020-04-07 18:23:05 +0300

    Created the class FilesCache

---
+++ app/src/main/java/com/technion/vedibarta/utilities/filesCaching/FilesCache.kt
@@ -0,0 +1,46 @@
+package com.technion.vedibarta.utilities.filesCaching
+
+import android.content.Context
+import java.io.File
+
+class FilesCache(context: Context, relativePath: String) {
+    private val folder = context.filesDir.resolve(relativePath)
+    private val filesMap = filesMaps.getOrPut(relativePath) { mutableMapOf() }
+
+    companion object {
+        // A set of cache folders that were reset
+        private var resetSet = mutableSetOf<String>()
+        // A map from cache folders to their files map
+        private var filesMaps = mutableMapOf<String, MutableMap<String, File>>()
+    }
+
+    init {
+        if (relativePath !in resetSet) {
+            clear()
+            resetSet.add(relativePath)
+        }
+
+        if (!folder.isDirectory)
+            folder.mkdirs()
+    }
+
+    fun newFile(fileName: String): File {
+        val file = File(folder, fileName).also { it.createNewFile() }
+        file.deleteOnExit()
+
+        filesMap[fileName] = file
+
+        return file
+    }
+
+    operator fun contains(fileName: String) = fileName in filesMap
+
+    operator fun get(fileName: String) = filesMap[fileName]
+
+    fun deleteFile(fileName: String) = filesMap.remove(fileName)?.delete()
+    fun deleteFile(file: File) = filesMap.values.remove(file)
+
+    fun clear() = folder.deleteRecursively()
+}
+
+fun FilesCache.getOrNew(fileName: String) = get(fileName) ?: newFile(fileName)
