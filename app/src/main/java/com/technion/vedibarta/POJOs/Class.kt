6963a5cf3043e06bc5b8d54938f6452b5315977b
Or Borenstein <borenstein@campus.technion.ac.il>
2020-04-15 00:10:29 +0300

    Edited the Class class

--- app/src/main/java/com/technion/vedibarta/POJOs/Class.kt
+++ app/src/main/java/com/technion/vedibarta/POJOs/Class.kt
@@ -1,16 +1,15 @@
 package com.technion.vedibarta.POJOs
 
+import android.os.Parcelable
 import com.google.firebase.firestore.DocumentId
+import kotlinx.android.parcel.Parcelize
 import java.io.Serializable
 
-/***
- * [Class] represents a class document in database
- * [id] is automatically populated by FireBase when document is read
- */
-data class Class(val name: String = "",
-                 val students: MutableList<StudentParticipant> = mutableListOf()): Serializable
-{
-    @DocumentId val id: String = ""
-
-    data class StudentParticipant(val id: String = "", var wasReported: Boolean = false): Serializable
-}
+@Parcelize
+data class Class(
+    val name: String = "",
+    @DocumentId
+    val id: String = "",
+    val studentsIDs: List<String> = emptyList(),
+    val teacherID: String = ""
+): Parcelable, Serializable
