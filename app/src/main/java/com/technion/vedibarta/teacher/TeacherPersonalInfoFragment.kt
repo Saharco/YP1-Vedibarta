1b5d58d1cc94b5c6a8a2b71535ce24d29e77767b
victorbanyas <victan98@gmail.com>
2020-05-11 22:27:29 +0300

    Added RecyclerView for schools in teacher setup. Needs fixing of item addition and removal.

--- app/src/main/java/com/technion/vedibarta/teacher/TeacherPersonalInfoFragment.kt
+++ app/src/main/java/com/technion/vedibarta/teacher/TeacherPersonalInfoFragment.kt
@@ -1,29 +1,39 @@
 package com.technion.vedibarta.teacher
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.technion.vedibarta.R
+import kotlinx.android.synthetic.main.fragment_teacher_personal_info.*
+import kotlinx.android.synthetic.main.teacher_setup_school_card.*
 
 class TeacherPersonalInfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    private var numOfSchools: Int = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teacher_personal_info, container, false)
+        val view = inflater.inflate(R.layout.fragment_teacher_personal_info, container, false)
+
+        val schoolList = view.findViewById<RecyclerView>(R.id.schoolList)
+        schoolList.isNestedScrollingEnabled = false
+        schoolList.layoutManager = LinearLayoutManager(activity)
+        val adapter = SchoolsAdapter(numOfSchools)
+        schoolList.adapter = adapter
+
+        return view
     }
 
     companion object {
@@ -43,4 +53,41 @@ class TeacherPersonalInfoFragment : Fragment() {
                 }
             }
     }
+}
+
+class SchoolsAdapter(private var numOfSchools: Int): RecyclerView.Adapter<SchoolsAdapter.SchoolsViewHolder>() {
+    private val TAG = "SchoolsAdapter"
+
+    private val schoolCardsList = mutableListOf<Int>()
+
+    class SchoolsViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val addSchool = v.findViewById<FloatingActionButton>(R.id.addSchoolButton)
+        val removeSchool = v.findViewById<ImageButton>(R.id.removeSchoolButton)
+        var garbage: Int = 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SchoolsViewHolder {
+        schoolCardsList.add(1)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.teacher_setup_school_card, parent, false)
+        return SchoolsViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: SchoolsViewHolder, position: Int) {
+        holder.addSchool.setOnClickListener {
+            Log.d(TAG, "numOfSchools: $numOfSchools")
+            numOfSchools++
+            schoolCardsList.add(1)
+            holder.garbage = schoolCardsList[position]
+            notifyDataSetChanged()
+        }
+        holder.removeSchool.setOnClickListener {
+            Log.d(TAG, "numOfSchools: $numOfSchools")
+            numOfSchools--
+            schoolCardsList.removeAt(position)
+            holder.garbage = schoolCardsList[position]
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun getItemCount() = numOfSchools
 }
