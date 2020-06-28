f34acefd2a005f659b9ed480ddad7adccbc708b7
gilzim <gilzi@campus.technion.ac.il>
2020-06-28 14:13:23 +0300

    A proper alert dialog appears when opening a dynamic link as student

--- app/src/main/java/com/technion/vedibarta/main/MainActivity.kt
+++ app/src/main/java/com/technion/vedibarta/main/MainActivity.kt
@@ -8,7 +8,9 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import androidx.lifecycle.observe
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
@@ -18,6 +20,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
+import com.technion.vedibarta.POJOs.Gender
 import com.technion.vedibarta.R
 import com.technion.vedibarta.data.viewModels.StudentClassViewModel
 import com.technion.vedibarta.fragments.UserProfileFragment
@@ -46,21 +49,8 @@ class MainActivity : VedibartaActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Firebase.dynamicLinks
-            .getDynamicLink(intent)
-            .addOnSuccessListener(this) {pendingDynamicLinkData ->
-                var deepLink: Uri? = null
-                if (pendingDynamicLinkData != null) {
-                    deepLink = pendingDynamicLinkData.link
-                }
-                if (user != null &&
-                    deepLink != null &&
-                    deepLink.getBooleanQueryParameter("classID", false)) {
-                    val classId = deepLink.getQueryParameter("classID")
-                    val viewModel by viewModels<StudentClassViewModel>()
-                    viewModel.addStudentToClass(classId)
-                }
-            }
+        handleDynamicLinks()
+
         setContentView(R.layout.activity_main)
         bottomNavigation.setupWithNavController(navController)
 
@@ -77,7 +67,6 @@ class MainActivity : VedibartaActivity(),
     }
 
 
-
     override fun onCameraUploadClicked(dialog: DialogFragment) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
             != PackageManager.PERMISSION_GRANTED
@@ -89,6 +78,51 @@ class MainActivity : VedibartaActivity(),
             )
         } else {
             startCameraActivity()
+        }
+    }
+
+    private fun handleDynamicLinks() {
+        val viewModel by viewModels<StudentClassViewModel>()
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+                if (user != null &&
+                    deepLink != null &&
+                    deepLink.getBooleanQueryParameter("classID", false)
+                ) {
+                    val classId = deepLink.getQueryParameter("classID")
+                    viewModel.addStudentToClass(classId)
+                }
+            }
+        viewModel.event.observe(this) { event ->
+            if (!event.handled) {
+                when (event) {
+                    is StudentClassViewModel.Event.ClassAddedFromInvite -> {
+                        val builder = AlertDialog.Builder(this)
+                        builder
+                            .setTitle(R.string.class_invite)
+                            .setMessage(R.string.successfully_joined_class)
+                            .setPositiveButton(R.string.accept_text) { _, _ -> }
+                            .create().show()
+                    }
+                    is StudentClassViewModel.Event.AlreadyInClass -> {
+                        val message =
+                            if (student!!.gender == Gender.MALE) R.string.already_in_class_m else R.string.already_in_class_f
+                        val builder = AlertDialog.Builder(this)
+                        builder
+                            .setTitle(R.string.class_invite)
+                            .setMessage(message)
+                            .setPositiveButton(R.string.accept_text) { _, _ -> }
+                            .create().show()
+                    }
+                }
+                event.handled = true
+
+            }
         }
     }
 
@@ -126,7 +160,9 @@ class MainActivity : VedibartaActivity(),
                 supportFragmentManager.findFragmentById(R.id.main_content)
             val fragment = navHostFragment!!.childFragmentManager.fragments[0]
             when (requestCode) {
-                REQUEST_CAMERA -> (fragment as? UserProfileFragment)?.checkImageAndUpload(selectedImage)
+                REQUEST_CAMERA -> (fragment as? UserProfileFragment)?.checkImageAndUpload(
+                    selectedImage
+                )
                 SELECT_IMAGE -> (fragment as? UserProfileFragment)?.checkImageAndUpload(data!!.data)
             }
         }
